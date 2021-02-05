82cbf4bf1b4d17b8323205bf4138a5c6c7dde53a
Carlos <carsandeu@gmail.com>
2021-02-05 11:23:09 +0000

    change to unsudo to change pi password, not root

--- script.sh
+++ script.sh
@@ -59,7 +59,7 @@ if [ $response == "y" ]
 
     if [ $response == "y" ]
         then
-            passwd
+            sudo -u passwd
             printf "You just changed your password\n"
     else 
         printf "You did not change your password\n"
